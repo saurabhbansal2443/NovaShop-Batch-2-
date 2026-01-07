0ccf2130807764dfaa3757deda254952365724f2
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-01-07 20:45:27 +0530

    Pagination

--- src/Components/ProductGrid.jsx
+++ src/Components/ProductGrid.jsx
@@ -3,9 +3,13 @@ import ProductCard from "./ProductCard";
 
 const ProductGrid = () => {
   const [productData, setProductData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   async function getData() {
-    let data = await fetch(`https://dummyjson.com/products`);
+    let skip = (currentPage - 1) * 20;
+    let data = await fetch(
+      `https://dummyjson.com/products?limit=20&skip=${skip}`
+    );
     let jsonData = await data.json();
     setProductData(jsonData.products);
   }
@@ -14,11 +18,54 @@ const ProductGrid = () => {
     getData();
   }, []);
 
+  function handlePageChange(page) {
+    setCurrentPage(page);
+  }
+
+  const activePageClass = "page-btn active";
+  const pageClass = "page-btn";
+
   return (
-    <div className="product-grid">
-      {productData.map((pData) => (
-        <ProductCard key={pData.id} data={pData} />
-      ))}
+    <div>
+      <div className="product-grid">
+        {productData.map((pData) => (
+          <ProductCard key={pData.id} data={pData} />
+        ))}
+      </div>
+      <div className="pagination">
+        <button
+          onClick={() => {
+            handlePageChange(1);
+          }}
+          className={currentPage == 1 ? activePageClass : pageClass}
+        >
+          1
+        </button>
+        <button
+          onClick={() => {
+            handlePageChange(2);
+          }}
+          className={currentPage == 2 ? activePageClass : pageClass}
+        >
+          2
+        </button>
+        <button
+          onClick={() => {
+            handlePageChange(3);
+          }}
+          className={currentPage == 3 ? activePageClass : pageClass}
+        >
+          3
+        </button>
+        <button
+          onClick={() => {
+            handlePageChange(4);
+          }}
+          className={currentPage == 4 ? activePageClass : pageClass}
+        >
+          4
+        </button>
+      </div>
     </div>
   );
 };
